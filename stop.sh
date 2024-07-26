efc0ef82ce126076b4bedd73814209ddc84b5ddd
iam-raja <rajapeta45@gmail.com>
2024-07-26 18:42:09 +0530

    shell-robo

--- stop.sh
+++ stop.sh
@@ -5,4 +5,4 @@ echo "after calling Ec2.sh script, calling::$Instances"
 
 echo "Terminating-instnaces_id's: $Instances"
 
-aws ec2 terminate-instances --instance-ids $Instances
+# aws ec2 terminate-instances --instance-ids $Instances
