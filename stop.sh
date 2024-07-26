36bdf5b3f5352552107603fa5883d700d516510b
iam-raja <rajapeta45@gmail.com>
2024-07-26 18:32:50 +0530

    shell-robo

--- stop.sh
+++ stop.sh
@@ -3,6 +3,6 @@
 
 Instances=$(aws ec2 describe-instances --query "Reservations[0].Instances[0].{Instance:InstanceId}" --output text)
 
-echo "Terminating-instnaces_id's: $Instance"
+echo "Terminating-instnaces_id's: $Instances"
 
-aws ec2 terminate-instances --instance-ids $Instance
+aws ec2 terminate-instances --instance-ids $Instances
